2c1baaed6e3dbb03862dec34ef255db2a6ec5cc1
hibikihsh <hydrogen1121@gmail.com>
2024-11-06 17:54:36 +0900

    remove: vike for ssg

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import react from '@vitejs/plugin-react-swc'
-import vike from 'vike/plugin'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), vike()],
+	plugins: [react()],
 })
