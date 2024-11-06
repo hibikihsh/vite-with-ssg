32727bc0c77f1af7b5c63f388626edd74728390d
hibikihsh <hydrogen1121@gmail.com>
2024-11-06 16:21:00 +0900

    yarn add: vike for ssg

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import vike from 'vike/plugin'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react(), vike()],
 })
